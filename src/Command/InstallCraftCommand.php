36fab83a7217f89d32ef4588506a232802d40005
Rob Sanchez <rob@robsanchez.com>
2017-04-03 12:16:30 -0500

    Split install into two commands

--- src/Command/InstallCraftCommand.php
+++ src/Command/InstallCraftCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class InstallCraftCommand extends Command implements ExemptFromBootstrapInterface
+class InstallCraftCommand extends DownloadCraftCommand
 {
     /**
     * {@inheritdoc}
@@ -28,44 +28,9 @@ class InstallCraftCommand extends Command implements ExemptFromBootstrapInterfac
     /**
     * {@inheritdoc}
     */
-    protected function getArguments()
-    {
-        return array(
-            array(
-                'path',
-                InputArgument::OPTIONAL,
-                'Specify an installation path. Defaults to the current working directory.',
-            ),
-        );
-    }
-
-    /**
-    * {@inheritdoc}
-    */
     protected function getOptions()
     {
-        return array(
-            array(
-                'terms', // name
-                't', // shortcut
-                InputOption::VALUE_NONE, // mode
-                'I agree to the terms and conditions (https://buildwithcraft.com/license)', // description
-                null, // default value
-            ),
-            array(
-                'public', // name
-                'p', // shortcut
-                InputOption::VALUE_REQUIRED, // mode
-                'Rename the public folder.', // description
-                null, // default value
-            ),
-            array(
-                'overwrite', // name
-                'o', // shortcut
-                InputOption::VALUE_NONE, // mode
-                'Overwrite existing installation.', // description
-                null, // default value
-            ),
+        return array_merge(parent::getOptions(), array(
             array(
                 'site-name', // name
                 null, // shortcut
@@ -158,98 +123,87 @@ class InstallCraftCommand extends Command implements ExemptFromBootstrapInterfac
                 null, // default value
             ),
             array(
-                'download-only', // name
+                'create-db', // name
                 null, // shortcut
                 InputOption::VALUE_NONE, // mode
-                'Skip Craft CMS installer.', //description
+                'Create new database.', //description
             ),
-            array(
-                'skip-create-db', // name
-                null, // shortcut
-                InputOption::VALUE_NONE, // mode
-                'Assume existing database.', //description
-            ),
-
-        );
+        ));
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $start = microtime(true);
+    protected function fire()
+    {
+        parent::fire();
 
-        $this->path = rtrim($this->argument('path'), DIRECTORY_SEPARATOR) ?: getcwd();
+        // Create database
+        if ($this->option('create-db')) {
+            $command = $this->getApplication()->find('db:create');
 
-        try {
-            // 1. Download Craft CMS
-            //   $this->download();
-
-            if ($this->option('download-only')) {
-                return;
-            }
-
-            // 2. Create database
-            if (!$this->option('skip-create-db')) {
-                $command = $this->getApplication()->find('db:create');
-
-                $arguments = array(
-                    'command' => 'db:create',
-                    '--host'    => $this->option('db-host'),
-                    '--name'    => $this->option('db-name'),
-                    '--port'    => $this->option('db-port'),
-                    '--user'    => $this->option('db-user'),
-                    '--password' => $this->option('db-password'),
-                    '--admin-user' => $this->option('db-admin-user'),
-                    '--admin-password' => $this->option('db-admin-password'),
-                );
-
-                $inputs = new ArrayInput($arguments);
-                if ($command->run($inputs, $output) !== 0) {
-                    throw new Exception("Command db:create failed.");
-                }
-            }
-
-            // 3. Install Craft CMS
-            // Creates a dummy license.key file for  validations to pass.
-            // This key needs to be deleted after installation. Otherwise, Craft
-            // update and licensing functions won't work properly.
-            // Creates a db.config in craft/config
-            exec("touch {$this->path}/craft/config/license.key", $output, $status);
-            $this->generateDbConfig();
-
-            $this->getApplication()->bootstrap();
-
-            //  The SERVER_NAME needs to defined for intallation to pass
-            $_SERVER['SERVER_NAME'] = '';
-            $inputs = array(
-                'username' => $this->option('admin-user'),
-                'email' => $this->option('admin-email'),
-                'password' => $this->option('admin-password'),
-                'siteName' => $this->option('site-name'),
-                'siteUrl' => $this->option('site-url'),
-                'locale' => $this->option('locale')
+            $arguments = array(
+                'command' => 'db:create',
+                'db-name'   => $this->option('db-name'),
+                '--host'    => $this->option('db-host'),
+                '--port'    => $this->option('db-port'),
+                '--user'    => $this->option('db-user'),
+                '--password' => $this->option('db-password'),
+                '--admin-user' => $this->option('db-admin-user'),
+                '--admin-password' => $this->option('db-admin-password'),
             );
-            craft()->install->run($inputs);
 
-            exec("rm -f {$this->path}/craft/config/license.key", $output, $status);
+            $inputs = new ArrayInput($arguments);
 
-        }
-        catch(Exception $e) {
-            $this->error($e->getMessage());
-            return -1;
+            if ($command->run($inputs, $this->output) !== 0) {
+                throw new Exception('Command db:create failed.');
+            }
         }
 
-        if ($this->showsDuration) {
-            $output->writeln(sprintf('<info>Took %d seconds.</info>', microtime(true) - $start));
+        // 3. Install Craft CMS
+        // Creates a dummy license.key file for  validations to pass.
+        // This key needs to be deleted after installation. Otherwise, Craft
+        // update and licensing functions won't work properly.
+        // Creates a db.config in craft/config
+        touch($this->getPath().'/craft/config/license.key');
+
+        $this->generateDbConfig();
+
+        $this->getApplication()->setCraftPath($this->getPath().'/craft');
+
+        $craft = $this->getApplication()->bootstrap(true);
+
+        //  The SERVER_NAME needs to defined for intallation to pass
+        if (!isset($_SERVER['SERVER_NAME'])) {
+            $_SERVER['SERVER_NAME'] = 'Craft';
         }
 
-        return;
+        $installerParams = array(
+            'username' => $this->option('admin-user'),
+            'email' => $this->option('admin-email'),
+            'password' => $this->option('admin-password'),
+            'siteName' => $this->option('site-name'),
+            'siteUrl' => $this->option('site-url'),
+            'locale' => $this->option('locale')
+        );
+
+        $this->comment('Installing Craft...');
+
+        $craft->setIsNotInstallingAfterNextCheck();
+
+        $this->suppressOutput(function () use ($craft, $installerParams) {
+            $craft->install->run($installerParams);
+        });
+
+        unlink($this->getPath().'/craft/config/license.key');
+
+        $this->info('Craft installed.');
     }
 
-    protected function generateDbConfig() {
+    protected function generateDbConfig()
+    {
         $handlebars = new Handlebars(array(
             'loader' => new FilesystemLoader(__DIR__.'/../templates/'),
         ));
 
-        $destination = $this->path.'/craft/config/db.php';
+        $destination = $this->getPath().'/craft/config/db.php';
 
         $handle = fopen($destination, 'w');
 
@@ -264,63 +218,5 @@ class InstallCraftCommand extends Command implements ExemptFromBootstrapInterfac
         fwrite($handle, $output);
 
         fclose($handle);
-    }
-
-
-    /**
-    * {@inheritdoc}
-    */
-    protected function download()
-    {
-        $path = $this->path;
-        if (! is_dir($path) && ! @mkdir($path, 0777, true)) {
-            $this->error(sprintf('Could not create directory %s.', $path));
-
-            return;
-        }
-
-        // check terms and conditions
-        if (! $this->option('terms') && ! $this->confirm('I agree to the terms and conditions (https://buildwithcraft.com/license)')) {
-            $this->error('You did not agree to the terms and conditions (https://buildwithcraft.com/license)');
-
-            return;
-        }
-
-        // check if craft is already installed, and overwrite option
-        if (file_exists($path.'/craft') && ! $this->option('overwrite')) {
-            $this->error('Craft is already installed here!');
-
-            if (! $this->confirm('Do you want to overwrite?')) {
-                $this->info('Exited without installing.');
-
-                return;
-            }
-        }
-
-        $url = 'https://buildwithcraft.com/latest.tar.gz?accept_license=yes';
-
-        $this->comment('Downloading...');
-
-        $downloader = new TempDownloader($url, '.tar.gz');
-
-        $downloader->setOutput($this->output);
-
-        $filePath = $downloader->download();
-
-        $this->comment('Extracting...');
-
-        $tarExtractor = new TarExtractor($filePath, $path);
-
-        $tarExtractor->extract();
-
-        // change the name of the public folder
-        if ($public = $this->option('public')) {
-            rename($path.'/public', $path.'/'.$public);
-        }
-
-        // Rename .htaccess
-        rename($path.'/public/htaccess', $path.'/'.$public.'/.htaccess');
-
-        $this->info('Installation complete!');
     }
 }
