0f289d377b6125ca29218047adeda23424659a9a
Rob Sanchez <rob@robsanchez.com>
2017-07-13 14:45:14 -0400

    Release 0.14.1

--- src/Application.php
+++ src/Application.php
@@ -30,7 +30,7 @@ class Application extends ConsoleApplication
     /**
      * Symfony Console Application version
      */
-    const VERSION = '0.14.0';
+    const VERSION = '0.14.1';
 
     /**
      * Default configuration file name
